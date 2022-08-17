4a38d2536331b87f445e5b3dae187ce4eb56bd1c
Ryan Hoover <ryan.hoover1618@gmail.com>
2022-08-17 17:28:04 -0400

    update reference.py

--- reference.py
+++ reference.py
@@ -1,3 +1,24 @@
-x = "Hello, World!"
-print(x)
+# Hello world message.
+# Variables
+# May only contain letters, number, and underscores- cannot start with number.
+message = "Hello, World!"
+print(message)
 
+message = "Hello, Python Crash Course!"
+print(message)
+
+
+# Strings- anything inside single or double quotes.
+# Changing case in a string.
+name = "ryan hoover"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+# Using variables in strings
+first_name = "Ryan"
+last_name = "Hoover"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
