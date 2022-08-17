47f67433543a18a58294b69b1ff947e865bfe40d
Ryan Hoover <ryan.hoover1618@gmail.com>
2022-08-17 17:42:26 -0400

    added changes to reference.py

--- reference.py
+++ reference.py
@@ -22,3 +22,17 @@ full_name = f"{first_name} {last_name}"
 print(f"Hello, {full_name.title()}!")
 message = f"Hello, {full_name.title()}!"
 print(message)
+
+# Adding whitespace with Tabs or Newlines
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+#Stripping whitespace
+favorite_language = "Python "
+print(favorite_language)
+print(favorite_language.rstrip())
+favorite_language = " Python"
+print(favorite_language.lstrip())
+favorite_language = " Python "
+print(favorite_language.strip())
